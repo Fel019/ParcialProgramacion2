2598ef6b2739d9bf51ae18b012b5e5bc42809d40
Valentina <obonagav@gmail.com>
2025-04-17 21:41:37 -0500

    Calse Arma

---
+++ src/parcialprojohan/andres/Arma.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcialprojohan.andres;
+
+public class Arma {
+    private String nombre;
+    private int dañoAdicional;
+
+    public Arma(String nombre, int dañoAdicional) {
+        this.nombre = nombre;
+        this.dañoAdicional = dañoAdicional;
+    }
+
+    public int atacarConArma() {
+        return dañoAdicional;
+    }
+
+    public int getDañoAdicional() {
+        return dañoAdicional;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
