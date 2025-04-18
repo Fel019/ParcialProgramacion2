15515a43dc63fc82e919fd7e82a3de10f98d6141
Valentina <obonagav@gmail.com>
2025-04-17 21:39:46 -0500

    Clase Volador

---
+++ src/parcialprojohan/andres/Volador.java
@@ -0,0 +1,11 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcialprojohan.andres;
+
+public interface Volador {
+    void volar();
+    void aterrizar();
+}
